417cd9760628b5d15988f92961e32d7bf6a25b29
Fany E <fanyervansyah.9c@gmail.com>
2019-11-01 21:12:04 +0700

    feat(list-task):list of tasks with working button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 import Exam from "./pages/exam";
 import CreateTask from "./pages/createTask";
+import ListTask from "./pages/listTask";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -12,10 +13,6 @@ class App extends React.Component {
 
   handleMenuClick = (e, { name }) => {
     this.setState({ activeItem: name });
-
-    if (this.state.activeItem === true) {
-      return <CreateTask />;
-    }
   };
 
   render() {
@@ -26,6 +23,7 @@ class App extends React.Component {
             <Menu.Item as={Link} to="/" exact>
               <h3>Kuding</h3>
             </Menu.Item>
+            <Menu.Item as={Link} name="List Task" to="/" />
             <Menu.Item as={Link} name="Create Task" to="/createtask" />
             <Menu.Menu position="right">
               <Menu.Item
@@ -35,12 +33,9 @@ class App extends React.Component {
             </Menu.Menu>
           </Menu>
 
-          <Route path="/" exact>
-            <Exam />
-          </Route>
-          <Route path="/createtask">
-            <CreateTask />
-          </Route>
+          <Route path="/" exact component={ListTask} />
+          <Route path="/createtask" component={CreateTask} />
+          <Route path="/dotask" component={Exam} />
         </Router>
       </>
     );
---
+++ src/pages/listTask/index.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Segment, Button, Table, Icon } from "semantic-ui-react";
+import axios from "axios";
+
+export default class ListTask extends React.Component {
+  state = {
+    tasks: [],
+    isLoading: true
+  };
+
+  getList = () => {
+    this.setState({
+      isLoading: true
+    });
+    axios
+      .get("https://kuding-backend.herokuapp.com/task")
+      .then(res => {
+        this.setState({
+          tasks: res.data
+        });
+      })
+      .finally(() => {
+        this.setState({
+          isLoading: false
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.getList();
+  }
+
+  handleDeleteClick = _id => {
+    this.setState({
+      isLoading: true
+    });
+    axios
+      .delete("https://kuding-backend.herokuapp.com/task/" + _id)
+      .finally(() => {
+        this.getList();
+        alert("data berhasil dihapus!");
+      });
+  };
+
+  handleSolveClick = task => {
+    this.props.history.push("/dotask", { task });
+  };
+
+  render() {
+    return (
+      <Segment loading={this.state.isLoading} basic>
+        <Table celled striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colspan="4">Tasks</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          {this.state.tasks.map((task, index) => (
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Icon name="file code" />
+                  <b>{task.title}</b>
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <p
+                    style={{
+                      textOverflow: "ellipsis",
+                      overflow: "hidden",
+                      width: "400px"
+                    }}
+                  >{`${task.description}`}</p>
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <Button
+                    basic
+                    onClick={() => this.handleDeleteClick(task._id)}
+                    color="red"
+                  >
+                    Delete
+                  </Button>
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <Button
+                    basic
+                    onClick={() => this.handleSolveClick(task)}
+                    color="green"
+                  >
+                    Solve
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          ))}
+        </Table>
+      </Segment>
+    );
+  }
+}
