7772bb66ac5e3601b0ac18f880de3341f9d7b6da
M. Nindra Zaka <mnindrazaka@gmail.com>
2019-12-07 16:02:49 +0700

    feat(task): show username

--- src/pages/listTask/index.jsx
+++ src/pages/listTask/index.jsx
@@ -1,60 +1,65 @@
-import React from "react";
-import { Segment, Button, Table, Icon, Card } from "semantic-ui-react";
-import axios from "axios";
+import React from 'react'
+import { Segment, Button, Card, Header } from 'semantic-ui-react'
+import axios from 'axios'
+import jwt from 'jsonwebtoken'
 
 export default class ListTask extends React.Component {
   state = {
     tasks: [],
     isLoading: true
-  };
+  }
 
   getList = () => {
     this.setState({
       isLoading: true
-    });
+    })
     axios
-      .get("https://kuding-backend.herokuapp.com/task", {
+      .get('https://kuding-backend.herokuapp.com/task', {
         headers: {
-          authorization: `Bearer ${localStorage.getItem("token")}`
+          authorization: `Bearer ${localStorage.getItem('token')}`
         }
       })
       .then(res => {
         this.setState({
           tasks: res.data
-        });
+        })
       })
       .finally(() => {
         this.setState({
           isLoading: false
-        });
-      });
-  };
+        })
+      })
+  }
 
   componentDidMount() {
-    this.getList();
+    this.getList()
   }
 
   handleDeleteClick = _id => {
     this.setState({
       isLoading: true
-    });
+    })
     axios
-      .delete("https://kuding-backend.herokuapp.com/task/" + _id, {
+      .delete('https://kuding-backend.herokuapp.com/task/' + _id, {
         headers: {
-          authorization: `Bearer ${localStorage.getItem("token")}`
+          authorization: `Bearer ${localStorage.getItem('token')}`
         }
       })
       .finally(() => {
-        this.getList();
-        alert("data berhasil dihapus!");
-      });
-  };
+        this.getList()
+        alert('data berhasil dihapus!')
+      })
+  }
 
   handleSolveClick = task => {
-    this.props.history.push("/dotask", { task });
-  };
+    this.props.history.push('/dotask', { task })
+  }
 
   render() {
+    const token = localStorage.getItem('token')
+    const { username } = jwt.decode(token)
+    console.log(username)
+
     return (
       <Segment loading={this.state.isLoading} basic>
         <h1>Tasks</h1>
@@ -64,6 +69,9 @@ export default class ListTask extends React.Component {
               <Card.Header>{`Task ${index + 1}`}</Card.Header>
               <Card.Description>{task.description}</Card.Description>
             </Card.Content>
+            <Card.Content>
+              <Header icon="user" content={task.user.username} size="tiny" />
+            </Card.Content>
             <Card.Content extra>
               <Button
                 basic
@@ -72,20 +80,19 @@ export default class ListTask extends React.Component {
               >
                 Solve
               </Button>
-              <Button
-                basic
-                color="red"
-                onClick={() => this.handleDeleteClick(task._id)}
-              >
-                Delete
-              </Button>
+              {username === task.user.username && (
+                <Button
+                  basic
+                  color="red"
+                  onClick={() => this.handleDeleteClick(task._id)}
+                >
+                  Delete
+                </Button>
+              )}
             </Card.Content>
-            {/* <Card.Content>
-              {!task.user ? task.user.username : "Anonim"}
-            </Card.Content> */}
           </Card>
         ))}
       </Segment>
-    );
+    )
   }
 }
